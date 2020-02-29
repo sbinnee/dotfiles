0d9aec637d234b6bf61426630acf44091dbd80f7
Seongbin Lim <sungbin246@gmail.com>
2020-02-29 23:06:26 +0100

    image metadata preview

--- .config/lf/pv.sh
+++ .config/lf/pv.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
-case "$1" in
-	*.tar*) tar tf "$1";;
-	*.zip) unzip -l "$1";;
-	*.pdf) pdftotext "$1" -;;
+FILE_PATH="${1}"
+FILE_EXTENSION="${FILE_PATH##*.}"
+FILE_EXTENSION_LOWER=$(echo ${FILE_EXTENSION} | tr '[:upper:]' '[:lower:]')
+
+case "${FILE_EXTENSION_LOWER}" in
+	tar*) tar tf "$1";;
+	zip) unzip -l "$1";;
+	pdf) pdftotext "$1" -;;
+	jpeg|jpg|png|gif) exiv2 "$1";;
 	*) highlight -O ansi "$1" || cat "$1";;
 esac
 
