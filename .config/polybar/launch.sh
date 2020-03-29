60bf69c55763e5db07b983f184eaa766bc54ab68
Seongbin Lim <sungbin246@gmail.com>
2020-03-29 20:56:49 +0900

    i3: border 3, polybar delete example

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,7 +7,7 @@ killall -q polybar
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log
-polybar example >> /tmp/polybar1.log 2>&1 &
+polybar unicorn >> /tmp/polybar1.log 2>&1 &
  
 # echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 # polybar bar1 >>/tmp/polybar1.log 2>&1 &
