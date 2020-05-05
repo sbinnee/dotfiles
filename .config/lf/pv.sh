9957b7a63f1b3ca11a057a5fdc4a30e576d79752
Seongbin Lim <sungbin246@gmail.com>
2020-05-05 20:58:08 +0900

    Preview markdown with glow in lf

--- .config/lf/pv.sh
+++ .config/lf/pv.sh
@@ -7,6 +7,7 @@ FILE_EXTENSION_LOWER=$(echo ${FILE_EXTENSION} | tr '[:upper:]' '[:lower:]')
 case "${FILE_EXTENSION_LOWER}" in
 	*tar*|tgz) tar tf "$1";;
 	zip) unzip -l "$1";;
+	md) glow -s dark "$1";;
 	pdf) pdftotext "$1" -;;
 	jpeg|jpg|png|gif) exiv2 "$1";;
 	*) highlight -O ansi "$1" || cat "$1";;
