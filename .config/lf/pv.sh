cf7cf50c438bf99f4f54be6e0bd82ff5bea2d84f
seongbin lim <sungbin246@gmail.com>
2020-06-04 22:22:22 +0200

    Minor preview change

--- .config/lf/pv.sh
+++ .config/lf/pv.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
-
 FILE_PATH="${1}"
 # FILE_EXTENSION="${FILE_PATH#*.}"
 FILE_EXTENSION="${FILE_PATH##*.}"
 FILE_EXTENSION_LOWER=$(echo ${FILE_EXTENSION} | tr '[:upper:]' '[:lower:]')
-printf "FileType: .%s\n---\n" "$FILE_EXTENSION_LOWER"
+# printf "FileType: .%s\n---\n" "$FILE_EXTENSION_LOWER"
 
 case "${FILE_EXTENSION_LOWER}" in
 	tar|tgz|xz|gz) tar tf "$1";;
 	zip) unzip -l "$1";;
-	md) glow -s dark "$1";;
+	md) glow -s dark "$1" -w 72;;
 	pdf) pdftotext "$1" -;;
 	jpeg|jpg|png|gif) exiv2 "$1";;
 	# mkv|webm|mp4) mediainfo "$1" | tr -d '[:blank:]';;
