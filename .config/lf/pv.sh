c9445b21663390ca11dad15dfc99f11778423292
seongbin lim <sungbin246@gmail.com>
2020-06-10 18:32:08 +0200

    Chafa image preview and typo

--- .config/lf/pv.sh
+++ .config/lf/pv.sh
@@ -10,7 +10,8 @@ case "${FILE_EXTENSION_LOWER}" in
 	zip) unzip -l "$1";;
 	md) glow -s dark "$1" -w 72;;
 	pdf) pdftotext "$1" -;;
-	jpeg|jpg|png|gif) exiv2 "$1";;
+	jpeg|jpg|png|gif) chafa -w 1 -c 240 -s 40x13 "$1"; exiv2 "$1";;
+	# jpeg|jpg|png|gif) exiv2 "$1";;
 	# mkv|webm|mp4) mediainfo "$1" | tr -d '[:blank:]';;
 	mkv|webm|mp4) mediainfo "$1" | grep -P 'Complete name|File size|Duration|Width|Height|Display aspect ratio' | tr -d '[:blank:]' | sed 's/:/   \t/';;
 	*) highlight -O ansi "$1" || cat "$1";;
