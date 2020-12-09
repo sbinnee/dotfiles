773c46b8845c53ebcfa540ce7db7d66a1af4f23b
seongbin lim <sungbin246@gmail.com>
2020-12-09 15:59:51 +0900

    Set up firefox!

---
+++ .config/firefox/README.md
@@ -0,0 +1,12 @@
+# Firefox setting
+Copy or make a soft link to ~/.mozilla/firefox/$PROFILE/user.js  
+PROFILE=xxxxxx.default-release by default
+1. Hardware acceleration
+2. Smooth scrolling
+
+## Hardware acceleration
+Follow arch wiki! [link](https://wiki.archlinux.org/index.php/Firefox#Hardware_video_acceleration)
+
+## Smooth scrolling
+- [link](https://www.reddit.com/r/firefox/comments/9a7oav/turning_smoothscrolling_off_makes_navigation_feel/e4urhip/
+)
---
+++ .config/firefox/user.js
@@ -0,0 +1,15 @@
+// Mozilla User Preferences
+
+// Scrolling
+user_pref("mousewheel.default.delta_multiplier_y", 50);
+user_pref("mousewheel.transaction.ignoremovedelay", 16);
+user_pref("mousewheel.min_line_scroll_amount", 1);
+user_pref("general.smoothScroll.msdPhysics.enabled", true);
+user_pref("general.smoothScroll.msdPhysics.motionBeginSpringConstant", 50);
+user_pref("general.smoothScroll.msdPhysics.regularSpringConstant", 100);
+user_pref("general.smoothScroll.msdPhysics.slowdownSpringConstant", 100);
+
+// Hardware acceleration
+user_pref("media.ffmpeg.vaapi.enabled", true);
+user_pref("media.ffvpx.enabled", false);
+user_pref("media.av1.enabled", false);
