c91cbb765c9b419d1c96e98b5c056e3cc355f15e
seongbin lim <sungbin246@gmail.com>
2021-01-14 05:25:11 +0900

    Fix firefox GPU acceleration setting

--- .config/firefox/user.js
+++ .config/firefox/user.js
@@ -4,6 +4,7 @@
 user_pref("mousewheel.default.delta_multiplier_y", 50);
 user_pref("mousewheel.transaction.ignoremovedelay", 16);
 user_pref("mousewheel.min_line_scroll_amount", 1);
+user_pref("general.smoothScroll.mouseWheel.durationMinMS", 10);
 user_pref("general.smoothScroll.msdPhysics.enabled", true);
 user_pref("general.smoothScroll.msdPhysics.motionBeginSpringConstant", 50);
 user_pref("general.smoothScroll.msdPhysics.regularSpringConstant", 100);
@@ -11,6 +12,7 @@ user_pref("general.smoothScroll.msdPhysics.slowdownSpringConstant", 100);
 
 // Hardware acceleration
 // Don't forget to set ENV variable!
+user_pref("gfx.webrender.all", true);
 user_pref("media.ffmpeg.vaapi.enabled", true);
 user_pref("media.ffvpx.enabled", false);
 user_pref("media.av1.enabled", false);
