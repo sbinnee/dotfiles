bbc3fa98b77e67cce251c975d67083b5f2c7d9d1
seongbin lim <sungbin246@gmail.com>
2021-02-20 22:06:53 +0100

    Add wmv preview

--- .config/lf/pv.sh
+++ .config/lf/pv.sh
@@ -14,7 +14,7 @@ case "${FILE_EXTENSION_LOWER}" in
 	jpeg|jpg|png|gif) chafa -w 1 -c 240 -s 40x13 "$1"; exiv2 "$1";;
 	# jpeg|jpg|png|gif) exiv2 "$1";;
 	# mkv|webm|mp4) mediainfo "$1" | tr -d '[:blank:]';;
-	mkv|webm|mp4) mediainfo "$1" | grep -P 'Complete name|File size|Duration|Width|Height|Display aspect ratio' | tr -d '[:blank:]' | sed 's/:/   \t/';;
+	mkv|webm|mp4|wmv) mediainfo "$1" | grep -P 'Complete name|File size|Duration|Width|Height|Display aspect ratio' | tr -d '[:blank:]' | sed 's/:/   \t/';;
 	m4a|aac|opus) mediainfo "$1" | tr -d '[:blank:]' | sed 's/:/   \t/';;
 	*) highlight -O ansi "$1" || cat "$1";;
 esac
