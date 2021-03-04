dd770c1e3b8488ec3915ccc4e9503dea9e491c9d
seongbin lim <sungbin246@gmail.com>
2021-03-04 22:05:03 +0100

    Fix mouse right click and some more settings
    Respecting wm when fullscreen

--- .config/firefox/user.js
+++ .config/firefox/user.js
@@ -1,5 +1,7 @@
 // Mozilla User Preferences
 
+user_pref("browser.fullscreen.autohide", false);
+
 // Scrolling
 user_pref("mousewheel.default.delta_multiplier_y", 50);
 user_pref("mousewheel.default.delta_multiplier_x", 300);
@@ -13,7 +15,14 @@ user_pref("general.smoothScroll.msdPhysics.slowdownSpringConstant", 100);
 
 // Hardware acceleration
 // Don't forget to set ENV variable!
+user_pref("layers.acceleration.force-enabled", true);
 user_pref("gfx.webrender.all", true);
 user_pref("media.ffmpeg.vaapi.enabled", true);
-user_pref("media.ffvpx.enabled", false);
+user_pref("media.ffmpeg.vaapi-drm-display.enabled", true);
+user_pref("media.ffvpx.enabled", true);
 user_pref("media.av1.enabled", false);
+
+// Right mouse click
+// https://wiki.archlinux.org/index.php/Firefox#Right_mouse_button_instantly_clicks_the_first_option_in_window_managers
+user_pref("ui.context_menus.after_mouseup", true);
+user_pref("full-screen-api.ignore-widgets", true);
