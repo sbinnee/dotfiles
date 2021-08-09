62cb9e3cb9b2cdca1f8e8acd14ed3877d258cf19
seongbin lim <sungbin246@gmail.com>
2021-08-10 01:14:34 +0900

    Remove polybar fallback monitor

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -2,13 +2,13 @@
 
 # Terminate already running bar instances
 killall -q polybar
-# If all your bars have ipc enabled, you can also use 
+# If all your bars have ipc enabled, you can also use
 # polybar-msg cmd quit
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log
 polybar "$@" >> /tmp/polybar1.log 2>&1 &
- 
+
 # echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 # polybar bar1 >>/tmp/polybar1.log 2>&1 &
 # polybar bar2 >>/tmp/polybar2.log 2>&1 &
