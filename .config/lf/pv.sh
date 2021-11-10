c214962c3dc8dd8f224aef8ff075b9cce365583c
Seongbin Lim <sungbin246@gmail.com>
2021-11-10 21:28:59 +0100

    Update here and there

--- .config/lf/pv.sh
+++ .config/lf/pv.sh
@@ -6,16 +6,16 @@ FILE_EXTENSION_LOWER=$(echo ${FILE_EXTENSION} | tr '[:upper:]' '[:lower:]')
 # printf "FileType: .%s\n---\n" "$FILE_EXTENSION_LOWER"
 
 case "${FILE_EXTENSION_LOWER}" in
-	tar|tgz|xz|bz2|gz|zst) tar tf "$1";;
-	zip) unzip -l "$1";;
-	rar) unrar l "$1";;
-	md) glow -s dark "$1" -w 72;;
-	pdf) pdftotext "$1" -;;
-	jpeg|jpg|png|gif) chafa -w 1 -c 240 -s 40x13 "$1"; exiv2 "$1";;
+    tar|tgz|xz|bz2|gz|zst) tar tf "$1";;
+    zip) unzip -l "$1";;
+    rar) unrar l "$1";;
+    md) glow -s dark "$1" -w 72;;
+    pdf) pdftotext "$1" -;;
+    jpeg|jpg|png|gif) chafa -w 1 -c 240 -s 40x13 "$1"; exiv2 "$1";;
     tif|tiff) mediainfo "$1" | tr -d '[:blank:]' | sed 's/:/:\t/';;
-	# jpeg|jpg|png|gif) exiv2 "$1";;
-	# mkv|webm|mp4) mediainfo "$1" | tr -d '[:blank:]';;
-	mkv|webm|mp4|wmv) mediainfo "$1" | grep -P 'Complete name|File size|Duration|Width|Height|Display aspect ratio' | tr -d '[:blank:]' | sed 's/:/   \t/';;
-	m4a|aac|opus) mediainfo "$1" | tr -d '[:blank:]' | sed 's/:/   \t/';;
-	*) highlight -O ansi "$1" || cat "$1";;
+    # jpeg|jpg|png|gif) exiv2 "$1";;
+    # mkv|webm|mp4) mediainfo "$1" | tr -d '[:blank:]';;
+    mkv|webm|mp4|wmv) mediainfo "$1" | grep -P 'Complete name|File size|Duration|Width|Height|Display aspect ratio' | tr -d '[:blank:]' | sed 's/:/   \t/';;
+    m4a|aac|opus|m4b) mediainfo "$1" | tr -d '[:blank:]' | sed 's/:/   \t/';;
+    *) highlight -O ansi "$1" || cat "$1";;
 esac
--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,7 +7,7 @@ killall -q polybar
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log
-polybar "$@" >> /tmp/polybar1.log 2>&1 &
+MONITOR=eDP-1 polybar "$@" >> /tmp/polybar1.log 2>&1 &
 
 # echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 # polybar bar1 >>/tmp/polybar1.log 2>&1 &
