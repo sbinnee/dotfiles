d972221252c496ad4b43e3c964301a191b1ef576
Seongbin Lim <sungbin246@gmail.com>
2021-11-27 23:59:59 +0100

    Put polybar on external monitor
    Support for only one monitor

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -5,9 +5,17 @@ killall -q polybar
 # If all your bars have ipc enabled, you can also use
 # polybar-msg cmd quit
 
+query="$(xrandr --listactivemonitors)"
+num_active="$(printf "%s" "$query" | awk 'NR==1 {print $2}')"
+if [ "$num_active" -eq 1 ]
+then
+    MONITOR="$(printf "%s" "$query" | awk 'NR==2 {print $4}')"
+fi
+
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log
-MONITOR=eDP-1 polybar "$@" >> /tmp/polybar1.log 2>&1 &
+#polybar "$@" >> /tmp/polybar1.log 2>&1 &
+MONITOR="${MONITOR:-"eDP-1"}" polybar "$@" >> /tmp/polybar1.log 2>&1 &
 
 # echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 # polybar bar1 >>/tmp/polybar1.log 2>&1 &
