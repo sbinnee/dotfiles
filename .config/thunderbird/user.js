db90ff826c20429916e6f393c97320b37c04a613
Seongbin Lim <sungbin246@gmail.com>
2022-08-31 23:18:57 +0200

    fix(thunderbird): WM_CLASS changed after updating
    enlarge in general

--- .config/thunderbird/user.js
+++ .config/thunderbird/user.js
@@ -1,4 +1,4 @@
-// user_pref("layout.css.devPixelsPerPx", "1.25");
+user_pref("layout.css.devPixelsPerPx", "1.25");
 user_pref("mailnews.default_sort_order", "2");
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("network.protocol-handler.warn-external.http", true);
