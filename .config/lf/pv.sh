b6aec0ad79ba94947dd05641eade7e24ae2f0378
Seongbin Lim <sungbin246@gmail.com>
2023-04-16 22:00:08 +0900

    change(lf-pv): use bat for lf preview

--- .config/lf/pv.sh
+++ .config/lf/pv.sh
@@ -9,7 +9,8 @@ case "${FILE_EXTENSION_LOWER}" in
     tar|tgz|xz|bz2|gz|zst) tar tf "$1";;
     zip) unzip -l "$1";;
     rar) unrar l "$1";;
-    md) glow -s dark "$1" -w 72;;
+    md) bat --style=numbers --paging=never --color=always "$1";;
+    # md) glow -s dark "$1" -w 72;;
     pdf) pdftotext "$1" -;;
     jpeg|jpg|png|gif) chafa -w 1 -c 240 -s 40x13 "$1"; exiv2 "$1";;
     tif|tiff) mediainfo "$1" | tr -d '[:blank:]' | sed 's/:/:\t/';;
@@ -17,5 +18,6 @@ case "${FILE_EXTENSION_LOWER}" in
     # mkv|webm|mp4) mediainfo "$1" | tr -d '[:blank:]';;
     mkv|webm|mp4|wmv) print_mediainfo "$1";;
     mp3|m4a|aac|opus|m4b) mediainfo "$1" | tr -d '[:blank:]' | sed 's/:/   \t/';;
-    *) highlight -O ansi "$1" || cat "$1";;
+    *) bat --style=numbers --paging=never --color always "$1";;
+    # *) highlight -O ansi "$1" || cat "$1";;
 esac
