15b5e22c4f5ad26e86b393301f755c5f949d678d
Seongbin Lim <seongbin@norispace.com>
2023-04-28 10:54:24 +0900

    fix(nvim-lsp): swap jump-diagnostic nmaps

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -42,8 +42,8 @@ require'lint'.linters_by_ft = {
 }
 -- See: https://github.com/neovim/nvim-lspconfig/tree/54eb2a070a4f389b1be0f98070f81d23e2b1a715#suggested-configuration
 local opts = { noremap=true, silent=true }
-vim.keymap.set('n', '<space>]', vim.diagnostic.goto_prev, opts)
-vim.keymap.set('n', '<space>[', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', '<space>[', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', '<space>]', vim.diagnostic.goto_next, opts)
 vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
 -- Use an on_attach function to only map the following keys
