c564b2e6844eabf67cabdaff374dfdc904823ba9
Seongbin Lim <sungbin246@gmail.com>
2023-07-23 21:57:55 +0900

    change(nvim:signature): adjust window pos

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -111,7 +111,26 @@ require'lspconfig'.gopls.setup{
 require "lsp_signature".setup({
   bind = true,
   doc_lines = 0,
-  floating_window_off_y = 30,
+  -- floating_window_off_y = 30,
+  floating_window_off_x = 15, -- adjust float windows x position.
+  floating_window_off_y = function() -- adjust float windows y position. e.g. set to -2 can make floating window move up 2 lines
+    local linenr = vim.api.nvim_win_get_cursor(0)[1] -- buf line number
+    local pumheight = vim.o.pumheight
+    local winline = vim.fn.winline() -- line number in the window
+    local winheight = vim.fn.winheight(0)
+
+    -- window top
+    if winline - 1 < pumheight then
+      return -3
+      -- return pumheight
+    end
+
+    -- window bottom
+    if winheight - winline < pumheight then
+      return -pumheight
+    end
+    return -3
+  end,
   handler_opts = {
     border = "single"
   },
