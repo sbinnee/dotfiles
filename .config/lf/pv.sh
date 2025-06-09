472df6dfbd7faf165279aba13ab01c4884834146
Seongbin Lim <sungbin246@gmail.com>
2025-06-09 11:19:33 +0900

    update(lf): fix image preview
    * simplify preview json when the file is too large

--- .config/lf/pv.sh
+++ .config/lf/pv.sh
@@ -1,15 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 # image preview using sixel
-if pidof Hyprland > /dev/null
-then
-    case "$(file -Lb --mime-type -- "$1")" in
-        image/*)
-            chafa -f sixels -s "$2x$3" --animate off --polite on "$1"
-            exit 1
-            ;;
-    esac
-fi
+case "$(file -Lb --mime-type -- "$1")" in
+    image/*)
+        chafa -f sixels -s "$2x$3" --animate off --polite on "$1"
+        exit 1
+        ;;
+esac
 
 FILE_PATH="${1}"
 # FILE_EXTENSION="${FILE_PATH#*.}"
@@ -30,6 +27,8 @@ case "${FILE_EXTENSION_LOWER}" in
     # mkv|webm|mp4) mediainfo "$1" | tr -d '[:blank:]';;
     mkv|webm|mp4|wmv) print_mediainfo "$1";;
     mp3|m4a|aac|opus|m4b) mediainfo "$1" | tr -d '[:blank:]' | sed 's/:/   \t/';;
-    *) bat --style=numbers --paging=never --color always "$1";;
+    json) jq 'if type == "object" then keys elif type == "array" then .[] else empty end' "$1";;
+    # json) [ "$(stat -f '%z')" -lt 52428800 ] && bat "$1" || jq 'if type == "object" then keys elif type == "array" then .[] else empty end' "$1";;
+    *) bat --style=numbers --paging=never --color=always "$1";;
     # *) highlight -O ansi "$1" || cat "$1";;
 esac
