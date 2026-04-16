80072feb532afe51a16bf153718593ab4ca9b3bc
Seongbin Lim <sungbin246@gmail.com>
2026-04-16 15:22:31 +0900

    fmt(claude_notify.sh): center a few lines to pop

--- .local/bin/claude_notify.sh
+++ .local/bin/claude_notify.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# TODO
+# - [ ] Linux
+#
 # macOS Dialog Notification
 input=$(cat)
 
@@ -23,19 +26,41 @@ input=$(cat)
 #   "title": "Permission needed",
 #   "notification_type": "permission_prompt"
 # }
+#
+#
+center_dialog_message() {
+    local input_string="$1"
+    local target_width="${2:-60}" # Default width is 60
+
+    echo "$input_string" | awk -v w="$target_width" '{ 
+        # Calculate padding: (Target - Length) / 2 * Proportional Multiplier
+        pad = int((w - length($0)) / 2 * 1.6); 
+        if (pad < 0) pad = 0;
+        # Create the padded string
+        printf "%*s%s", pad, " ", $0 
+    }'
+}
+
 tmux_source="$(tmux display-message -p "[#S] #I:#W")"
 hook_event_name=$(jq -r '.hook_event_name' <<< $input)
 if [ "$hook_event_name" = "Stop" ]
 then
     last_assistant_message=$(jq -r '.last_assistant_message' <<< $input)
-    osascript -e 'display dialog "'"tmux: $tmux_source\n[$hook_event_name] ${last_assistant_message:0:80}"'" buttons {"Got it"} default button "Got it" with title "Claude Code"'
+    _msg_source="$(center_dialog_message "tmux: $tmux_source")"
+    _msg_event="$(center_dialog_message "[$hook_event_name]")"
+    _msg_break="$(center_dialog_message "---")"
+    osascript -e 'display dialog "'"$_msg_source\n$_msg_break\n$_msg_event\n${last_assistant_message:0:100}"'" buttons {"Got it"} default button "Got it" with title "Claude Code"'
 elif [ "$hook_event_name" = "Notification" ]
 then
     notification_type=$(jq -r '.notification_type' <<< $input)
+    _msg_source="$(center_dialog_message "tmux: $tmux_source")"
+    _msg_event="$(center_dialog_message "[$notification_type]")"
+    _msg_break="$(center_dialog_message "---")"
+    _msg=$(jq -r '.message' <<< $input)
     if [ "$notification_type" = "idle_prompt" ]
     then
-        osascript -e 'display notification "'"tmux: $tmux_source\n[$notification_type]"'" with title "Claude Code"'
+        osascript -e 'display notification "'"$_msg_source\n$_msg_break\n$_msg_event\n$_msg"'" with title "Claude Code"'
     else
-        osascript -e 'display dialog "'"tmux: $tmux_source\n[$notification_type]"'" buttons {"Got it"} default button "Got it" with title "Claude Code"'
+        osascript -e 'display dialog "'"$_msg_source\n$_msg_break\n$_msg_event\n$_msg"'" buttons {"Got it"} default button "Got it" with title "Claude Code"'
     fi
 fi
